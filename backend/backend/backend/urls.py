e3ba17bdb59ff4799f3aee064e1c7d1af368222a
Ferdaous Adermouch <fadermou@c4r2p9.1337.ma>
2024-07-18 22:36:12 +0100

    push

--- backend/backend/backend/urls.py
+++ backend/backend/backend/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from back_end.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('home/', homepage),
+    path('', homepage),
 ]
