413c9e24b16fb25982e4d65f4ae134d8e9739962
Ferdaous Adermouch <fadermou@c4r8p4.1337.ma>
2024-08-10 18:31:38 +0100

    push

--- backend/back_end/views.py
+++ backend/back_end/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from django.contrib.auth.forms import AuthenticationForm
 from . import models
+
 def homepage(request):
     if (models.USER.objects.all().count() < 2):
+        user0 = AuthenticationForm.username
         user = models.USER(username="hajar", gender="F", age=20, email="salam")
         user1 = models.USER(username="ferdaous", gender="F", age=20, email="hhhh")
         user.save()
@@ -16,6 +19,17 @@ def homepage(request):
     return render(request, 'home.html', context={'data': data})
     # return render(request, 'home.html', context=data)
 
+# def loginpage(request):
+#     form = AuthenticationForm(request, data=request.POST or None)
+#     next_url = request.GET.get('next', '')
+    
+#     if form.is_valid():
+#         # Perform login logic
+#         return redirect(next_url or '/')
+
+#     return render(request, 'login.html', {'form': form, 'next': next_url})
+
 # def js(request):
 #     return render(request, 'Node.js')
+
 
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -14,12 +14,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from back_end.views import *
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', homepage),
+    path("accounts/", include('django.contrib.auth.urls')),
+    # path('login/', login),
     # path('', js),
 ]
+
