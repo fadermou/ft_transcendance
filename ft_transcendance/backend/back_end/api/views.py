3ee9818283191cab2b17a553a73027a892437159
Macbook Air <macbookair@Macbooks-MacBook-Air-2.local>
2024-10-20 17:58:44 +0100

    push

--- ft_transcendance/backend/back_end/api/views.py
+++ ft_transcendance/backend/back_end/api/views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
+from django.contrib.auth.models import User
 
 
 @api_view(['GET'])
@@ -15,12 +16,21 @@ def getRoute(request):
 
 def check_login_status(request):
     print(request.session.items())
+    user = User.objects.get(username='fadermou')
 
-    print (request.user)
+    print('--------------')
+    print (user.username)
+    print('--------------')
     if request.user.is_authenticated:
         return JsonResponse({'isLoggedIn': True})
 
     return JsonResponse({'isLoggedIn': False})
+
+def user(request):
+    # user = User.objects.get(username=request.user)
+
+    return JsonResponse({'username': request.user.username,
+                        'email':request.user.email})
 
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
     @classmethod
--- ft_transcendance/backend/back_end/utils/save.py
+++ ft_transcendance/backend/back_end/utils/save.py
@@ -8,7 +8,11 @@ def save_and_login_user(request, user_data):
     user, created = User.objects.get_or_create(username=username)
 
     if created:
+        user.email = email
         user.save()
-
+    # Ensure that the email is updated if the user already exists and it is not set
+    if not user.email:
+        user.email = email
+        user.save()
     login(request, user, settings.BACKEND)
     return
